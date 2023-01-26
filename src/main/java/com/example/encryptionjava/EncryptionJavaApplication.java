40e92d41aae8f5daea0b51bb85a058e2564e8343
jht <1751263383@qq.com>
2023-01-26 14:57:49 +0800

    初始化后端项目

---
+++ src/main/java/com/example/encryptionjava/EncryptionJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.encryptionjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptionJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/encryptionjava/EncryptionJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encryptionjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
